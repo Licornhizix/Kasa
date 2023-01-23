fd85550086fa3b5427129a04b1e2c397a144d266
Licornhizix <licornhizix@gmail.com>
2023-01-23 18:56:49 +0100

    implementation de la page apropos

--- src/pages/Logements/index.jsx
+++ src/pages/Logements/index.jsx
@@ -1,7 +1,18 @@
-function Logements() {
+import React from "react";
+import Header from "../../components/Header";
+import headerLogement from "../../assets/headerLogement.png";
+import "../../styles/logement.css"
+import "../../styles/collapse.css"
+
+import Card from "../../components/Card";
+
+function Logements () {
     return (
-        <div>
-            <h1>Molly 🧮</h1>
+        <div className="location">
+            <Header
+            imageHome = {headerLogement}
+            />
+            <Card />
         </div>
     )
 }
